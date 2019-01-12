20aee56fef3c95a2991c88bfd2fd8c7cafb9bb0e
Vince Andrew Pimentel <contact@vincep.me>
2019-01-12 03:13:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(username: params[:username])
 
-    if user && !user.username.blank? && user.authenticate(params[:password])
+    if user && user.username.empty? && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/account"
     else
